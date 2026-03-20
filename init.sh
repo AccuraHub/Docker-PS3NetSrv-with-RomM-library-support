9f15d0bce342d5d119865f0d2d32b11479c0ecd5
AccuraHub <s.bialka@outlook.com>
2026-03-20 18:56:00 +0100

    Added an environment variables support

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-python /ps3netsrv/RomM-PS3NetSrv-Cross-Mapping-latest/RomM-PS3NetSrv-Cross-Mapping.py
-/ps3netsrv/ps3netsrv_linuxX86_64
+if [ "$SYNC_ROMM_PATH" = true ] ; then
+    python /ps3netsrv/RomM-PS3NetSrv-Cross-Mapping-latest/RomM-PS3NetSrv-Cross-Mapping.py
+fi
+
+/ps3netsrv/ps3netsrv_linuxX86_64 $PS3NETSRV_PORT
